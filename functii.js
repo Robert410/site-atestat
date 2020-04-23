cbdf481a108810b002623d4d0e16679828428743
Robert <roberthostiuc@gmail.com>
2020-04-23 14:55:58 +0300

    first commit

---
+++ functii.js
@@ -0,0 +1,15 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("removee").style.display ="none";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("removee").style.display = "block";
+  }
+  
+
+  function color(){
+    document.getElementById("harta").style.color="white";
+  }
